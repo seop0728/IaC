366f0aafb2d88eea84bd321342c98aa22a1f12de
Minseop SEONG <110884476+seop0728@users.noreply.github.com>
2026-09-11 13:10:55 +0900

    Create main.tf

---
+++ terraform/ch03/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-northeast-2"
+}
+
+resource "aws_instance" "instance" {
+  ami           = "ami-0e4ab31f1847c850c" 
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "PracticeInstance"
+  }
+}
